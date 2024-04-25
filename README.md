d371ff2108dedf0fb9655e41495352691415a07e
AffanImran1303 <affanimran1303@gmail.com>
2024-04-25 15:59:54 +0500

    Updated

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite
+
+This is just a simple React-vite app.
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className="mb-10">Hello World!</h1>
+    <h2 className="mb-5">Muhammad Affan Imran</h2>
+    <p>This is first step to master ReactJS!</p>
+    </>
+  )
+}
+
+export default App
